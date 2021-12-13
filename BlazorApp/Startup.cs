5b5708ca3ff4b1b960916528e946e82a31c9c55b
Damien Rolighed <damr@itu.dk>
2021-12-13 16:12:12 +0100

    tilføjet AddNewNode UI elementer

--- BlazorApp/Startup.cs
+++ BlazorApp/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BlazorApp.Data;
+using Blazored.Modal;
 
 namespace BlazorApp
 {
@@ -28,6 +29,7 @@ namespace BlazorApp
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddBlazoredModal();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
