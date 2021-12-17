124f87f4039c92613c0507768bfa3ef523c62c38
bluetooth-afk <58256651+bluetooth-afk@users.noreply.github.com>
2021-12-17 12:31:57 +0100

    refactorized til blazorwebassembly

---
+++ BlazorWebAssembly/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorWebAssembly;
+using Blazored.Modal;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredModal();
+
+await builder.Build().RunAsync();
