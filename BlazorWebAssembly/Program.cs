05acda7fcc52e8cc2ee7dd5a075d5cfe0ca1572b
bluetooth-afk <58256651+bluetooth-afk@users.noreply.github.com>
2021-12-20 13:09:23 +0100

    title med småt

--- BlazorWebAssembly/Program.cs
+++ BlazorWebAssembly/Program.cs
@@ -9,11 +9,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("iLit.Server", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+builder.Services.AddHttpClient("iLit.API", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-/*/ Supply HttpClient instances that include access tokens when making requests to the server project
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("iLit.Server"));
-
+// Supply HttpClient instances that include access tokens when making requests to the server project
+/*builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("iLit.API"));
+/*
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
